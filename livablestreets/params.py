e7dfc6e7abe3be6f8ccf2148a28f3aa06295c327
carlos vieira <carloshvieira2@gmail.com>
2022-06-01 13:12:31 +0200

    connected gcp loading of grid file

--- livablestreets/params.py
+++ livablestreets/params.py
@@ -20,14 +20,14 @@ AWS_BUCKET_TEST_PATH = "s3://wagon-public-datasets/taxi-fare-test.csv"
 
 ### GCP Storage - - - - - - - - - - - - - - - - - - - - - -
 
-BUCKET_NAME = 'wagon-data-871-vieira'
+BUCKET_NAME = 'wagon-data-871-vieira-livablestreets'
 
 ##### Data  - - - - - - - - - - - - - - - - - - - - - - - -
 
 # train data file location
 # /!\ here you need to decide if you are going to train using the provided and uploaded data/train_1k.csv sample file
 # or if you want to use the full dataset (you need need to upload it first of course)
-BUCKET_TRAIN_DATA_PATH = 'data/train_1k.csv'
+BUCKET_TRAIN_DATA_PATH = 'data/Berlin_grid_1000m.csv'
 
 ##### Training  - - - - - - - - - - - - - - - - - - - - - -
 
