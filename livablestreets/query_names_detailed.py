93b056fad7e7807c17d48d7a6eda47e624c6ce59
Laia Grobe <laia.gdla@gmail.com>
2022-06-06 14:50:05 +0200

    queries detailed

--- livablestreets/query_names_detailed.py
+++ livablestreets/query_names_detailed.py
@@ -1,11 +1,20 @@
+
+# 4 categories with key, values list for openstreetmap feature extraction
+# dictionaries determinating geometry type (node or ways)
+# dictionaries for determinig influece distance, for computing sigma (blurring)
+# final master dictionary with all the information 'master_query'
+
+
 '''-------------------------mobility------------------------------'''
 
 #Queries for mobility
+
 # Nodes
-public_transport = {'amenity':['bus_station'],
+public_transport_bus = {'amenity':['bus_station'],
                     'bus_bay':'',
-                   'highway':['bus_stop','platform'],
-                   'public_transport':['stop_position','platform','station','stop_area'],
+                   'highway':['bus_stop']}
+
+public_transport_rail = { 'public_transport':['stop_position','station','stop_area'],
                    'railway':['station','tram_stop', 'subway_entrance']}
 
 bike_infraestructure =  {'amenity':['bicycle_parking', 'bicycle_repair_station', 'bicycle_rental']}
@@ -18,6 +27,21 @@ cycle_paths = {'bicycle':['designated'],
 pedestrian = {'highway':['pedestrian','footway','living_street','corridor'],
                 'foot':['designated']}
 
+# Distance and sigma
+
+mobility = {
+            'node' : [ public_transport_bus , public_transport_rail, bike_infraestructure ],
+            'way' : [ pedestrian, cycle_paths ]
+            }
+
+mobility_distance = {
+                    'public_transport_bus' : 200 ,
+                    'public_transport_rail' : 500 ,
+                    'bike_infraestructure' : 50 ,
+                    'pedestrian' : 100 ,
+                    'cycle_paths' : 100
+                    }
+
 '''-------------------------social life---------------------------'''
 
 #Queries for social life
@@ -28,14 +52,26 @@ eating = {'amenity':['cafe','restaurant', 'food_court', 'ice_cream']}
 night_life = { 'amenity':['bar','pub','biergarten', 'nightclub', 'swingerclub', 'casino']}
 
 culture = {'amenity':['social_centre','theatre','public_bookcase',
-                      'fountain', 'events_venue', 'community_centre', 'cinema',
+                      'events_venue', 'community_centre', 'cinema',
                       'arts_centre', 'conference_centre']}
 
 community = {'office': ['association','charity', 'coworking',
                         'educational_institution', 'employment_agency', 'foundation',
                         'ngo', 'political_party', 'research']}
 
+# Distance and sigma
 
+social_life = {
+            'node' : [ eating, night_life, culture, community ],
+            'way' : []
+            }
+
+social_life_distance = {
+                    'eating' : 200 ,
+                    'night_life' : 300 ,
+                    'culture' : 500 ,
+                    'community' : 400 ,
+                    }
 
 '''------------------------activities-------------------------------'''
 
@@ -43,19 +79,38 @@ community = {'office': ['association','charity', 'coworking',
 #Queries for acitvities
 
 #Nodes
-health_care = {'amenity':['baby_hatch','clinic','dentist',
-                      'doctors', 'hospital', 'nursing_home', 'pharmacy',
-                      'social_facility', 'veterinary']}
+health_regional = {'amenity':['hospital', 'clinic','nursing_home', 'social_facility']}
 
-public_service = {'amenity':['courthouse','fire_station','police',
-                      'post_box', 'post_office', 'townhall']}
+health_local = {'amenity':['baby_hatch','dentist', 'doctors', 'pharmacy', 'veterinary']}
 
-education = {'amenity':['college','kindergarten','language_school',
-                      'library', 'music_school', 'school', 'university']}
+goverment = { 'amenity': ['townhall', 'courthouse']}
+
+public_service = {'amenity':[ 'fire_station','police' ]}
+
+post = {'amenity':[ 'post_box', 'post_office' ]}
+
+education = {'amenity':['college', 'music_school', 'school', 'university']}
+
+educational = { 'amenity' : ['kindergarten','language_school', 'library',]}
 
 economic = {'amenity':['atm', 'bank', 'bureau_de_change']}
 
+# Distance and sigma
+activities = {
+            'node' : [ health_regional, health_local, goverment, public_service, post, education, educational, economic],
+            'way' : []
+            }
 
+activities_distance = {
+                    'health_regional' : 1000 ,
+                    'health_local' : 300 ,
+                    'goverment' : 500 ,
+                    'public_service' : 400 ,
+                    'post' : 100,
+                    'education' : 500,
+                    'educational' : 200,
+                    'economic' : 100
+                    }
 
 '''-----------------------------comfort--------------------------'''
 
@@ -65,12 +120,12 @@ economic = {'amenity':['atm', 'bank', 'bureau_de_change']}
 # Nodes
 comfort_spots = {'amenity':['bbq','bench','dog_toilet',
                       '	drinking_water', 'give_box', 'shelter', 'shower',
-                      'toilets', 'water_point', 'watering_place']}
+                      'toilets', 'water_point', 'watering_place', 'fountain']}
 
-leisure_spots = {'leisure':['bandstand','bird_hide','dog_park',
-                      'firepit', 'swimming_pool', 'stadium', 'sports_centre',
-                      'pitch', 'picnic_table', 'fitness_centre'],
-                 'historic':''}
+leisure_spots = {'leisure':['bird_hide','dog_park', 'firepit', 'pitch', 'picnic_table'],
+                 'historic': ['']}
+
+leisure_mass = {'leisure':['bandstand', 'swimming_pool', 'stadium', 'sports_centre', 'fitness_centre']}
 
 # Ways
 
@@ -80,3 +135,28 @@ green = {'landuse':['grass','forest','orchard','allotments','cementery','flowerb
 
 water = {'natural':['water','beach'],
          'amenity':['fountain']}
+
+# Distance and sigma
+comfort = {
+            'node' : [ comfort_spots, leisure_spots, leisure_mass],
+            'way' : [ green, water ]
+            }
+
+comfort_distance = {
+                    'comfort_spots' : 100 ,
+                    'leisure_spots' : 200 ,
+                    'leisure_mass' : 500 ,
+                    'green' : 500 ,
+                    'water' : 500
+                    }
+
+
+'''--------------------------master query--------------------------'''
+
+
+master_query = {
+                mobility : mobility_distance ,
+                social_life : social_life_distance,
+                activities : activities_distance,
+                comfort : comfort_distance
+                }
