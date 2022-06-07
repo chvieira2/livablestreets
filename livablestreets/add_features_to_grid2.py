99294bad03db10922a7dafcd201c61253177bf29
carlos vieira <carloshvieira2@gmail.com>
2022-06-07 16:16:43 +0200

    tried Laia features didnt work

--- livablestreets/add_features_to_grid2.py
+++ livablestreets/add_features_to_grid2.py
@@ -1,25 +1,25 @@
 import pandas as pd
 import numpy as np
-# from sqlalchemy import column
 from livablestreets.utils import simple_time_tracker, get_file, save_file, min_max_scaler
+from livablestreets.FeatCount_blurrying import FeatCount_blurrying
 from shapely.geometry import Point, Polygon
 import matplotlib.path as mplPath
 import geopandas as gpd
 import shapely.speedups
 
-from livablestreets.query_names_detailed import master_query_complex
-
 
 ## 79.36s to complete 1000m grids
 ### With sjoin
-def features_into_list_points(file_name, location='berlin'):
+def features_into_list_points(file_name, location, lat='lat',lng='lon'):
     """ Receives a file name, download it from GCP (or local if exists).
         Iterates through column pairs and returns the corresponding points """
     # Get the feature df, create a list of points out for each feature
 
-    geojson_feature = get_file(file_name, local_file_path=f'data/{location}/Features',
-                            gcp_file_path = f'data/{location}/Features')
-    return gpd.GeoDataFrame(geojson_feature)
+    gdf = gpd.read_file(filename=f'livablestreets/data/{location}/Features/{file_name}')
+    print(f'loaded {file_name}')
+
+    gdf = gdf.explode(index_parts=True)
+    return gdf
 
 def grid_to_polygon(df_grid):
     """Receives a dataframe with coordinates columns and adds a column of respective polygons"""
@@ -49,8 +49,8 @@ def feature_cat_mean_score(df):
     return df
 
 @simple_time_tracker
-def integrate_all_features_counts(df_grid=None, file_name = None,
-                                    stepsize = 3000, location = 'berlin',
+def integrate_all_features_counts(stepsize, location,
+                                    df_grid=None, file_name = None,
                                     save_local=True, save_gcp=False):
     """ Receives the name of the file that is obtained from GCP (or local if available).
         Calls external function to create the grid polygon for each grid"""
@@ -66,57 +66,39 @@ def integrate_all_features_counts(df_grid=None, file_name = None,
     print('created polygons')
 
     # Get the list of points
-    df_master = master_query_complex()
+    points_activities_economic = features_into_list_points('shapes-mobility-public_transport_rail.geojson', location=location)
 
+    print('created points lists')
 
+    # Iterates through the polygons and ask how many points are inside
+    activities_economic_in_polygon = []
 
     total_grids=len(df_grid)
-
-    for index_grid, row_grid in df_grid.iterrows():
-        point_in_polygon = []
-        print(f'{index_grid+1}/{total_grids}', end='\r')
-
-        if row_grid['grid_in_location']:
+    for index, row in df_grid.iterrows():
+        print(f'{index+1}/{total_grids}', end='\r')
+        if row['grid_in_location']:
             polygon = gpd.GeoDataFrame(pd.DataFrame({'index_value':1,
-                                                    'geometry':df_grid.loc[index_grid, 'polygon']}, index=[1]), crs='wgs84')
+                                                     'geometry':df_grid.loc[index, 'polygon']}, index=[1]), crs='wgs84')
 
-            for index_points, row_points in df_master.iterrows():
-                filter_name = index_points
-                category = row_points['category']
-
-                points = features_into_list_points(f'shapes-{category}-{filter_name}.geojson' , location=location)
-                print(f'created points lists: filter_name')
-
-                print(points['coordinates'])
-
-
-            print(type(point_in_polygon), point_in_polygon)
-
-            df_grid[filter_name] = point_in_polygon
-
-
-
-
-            point_in_polygon.append(point_in_grid_counter(polygon, points))
-
-
+            activities_economic_in_polygon.append(point_in_grid_counter(polygon, points_activities_economic))
 
         else:
-            point_in_polygon.append(np.NaN)
+            activities_economic_in_polygon.append(np.NaN)
 
 
+    df_grid['activities_economic'] = activities_economic_in_polygon
 
-    ## Create the livability score
+    ## Blurry count information
     # Remove polygons to save space
     df_grid = df_grid.drop(columns=['polygon'])
 
-    # MinMax normalize all features
-    df_grid = min_max_scaler(df_grid)
-    print('Features were MinMax scaled')
+    # Apply blurrying function
+    # df_grid = FeatCount_blurrying(df=df_grid)
 
+    ## Create the livability score
     # Calculate the mean per category
     df_grid= feature_cat_mean_score(df_grid)
-    print('Category mean was calculated')
+    print('Categories mean were calculated')
 
     save_file(df_grid, file_name=f'FeatCount_{location}_grid_{stepsize}m.csv', local_file_path=f'data/{location}/WorkingTables', gcp_file_path = f'data/{location}/WorkingTables', save_local=save_local, save_gcp=save_gcp)
 
@@ -130,6 +112,4 @@ def integrate_all_features_counts(df_grid=None, file_name = None,
 
 
 if __name__ == '__main__':
-    df_grid = integrate_all_features_counts(stepsize = 3000, file_name='berlin_grid_3000m.csv')
-    # df_grid = integrate_all_features_counts(stepsize = 1000, file_name='berlin_grid_1000m.csv')
-    # df_grid = integrate_all_features_counts(stepsize = 100, file_name='berlin_grid_100m.csv')
+    df_grid = integrate_all_features_counts(location = 'berlin', stepsize = 1000, file_name='berlin_grid_1000m.csv')
