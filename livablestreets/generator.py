0bd79a36431b4326ad1fec58778b600412649ccb
carlos vieira <carloshvieira2@gmail.com>
2022-06-09 16:42:50 +0200

    added negative features

--- livablestreets/generator.py
+++ livablestreets/generator.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 
 class LivabilityMap(object):
-    def __init__(self, location, stepsize = 100, weights = (1,1,1,1)):
+    def __init__(self, location, stepsize = 5000, weights = [1,1,1,1,1]):
         """ This class puts together all processes to generate and plot the map with livability heatmap
             """
         self.df_grid = None
@@ -69,9 +69,9 @@ class LivabilityMap(object):
                 ## Integrates features count to grid
         if self.query_df is None:
             # launchs queries of gejson and csv files from local PBF
-            self.query_df = master_query()
+            # self.query_df = master_query()
             # self.query_df = master_query_complex()
-            # self.query_df = master_query_negative()
+            self.query_df = master_query_negative()
 
         distances = list(self.query_df['distance'])
         self.sigmas = [(0.5*distance)/self.stepsize for distance in distances]
@@ -119,13 +119,26 @@ class LivabilityMap(object):
         return self.df_grid_Livability
 
     def update_livability(self, imputed_weights):
-        self.weights = imputed_weights
+        """ Calculate the livability score given the weights"""
 
-        # Check if livability has been calculated
+        # Get weights
+        if imputed_weights is None:
+            if self.weights is None:
+                self.weights_input()
+        else:
+            self.weights = imputed_weights
+
+        ## Calculate livability
         if self.df_grid_Livability is None:
-            self.df_grid_Livability = self.calc_livability()
+            try :
+                self.df_grid_Livability = get_file(f'Livability_{self.location}_grid_{self.stepsize}m.csv', local_file_path=f'livablestreets/data/{self.location}/WorkingTables', gcp_file_path = f'data/{self.location}/WorkingTables', save_local=True)
 
-        # update livability
+            except FileNotFoundError:
+                self.df_grid_Livability = livability_score(self.add_FeatCount_grid(), weights = self.weights,
+                                    stepsize = self.stepsize, location = self.location)
+        else:
+            print('livability_score has already been called before')
+
         if imputed_weights is not None:
             self.df_grid_Livability = livability_score(self.df_grid_Livability,
                                                         weights = self.weights,
@@ -133,70 +146,49 @@ class LivabilityMap(object):
                             save_local=True, save_gcp=False)
             print(f'Livability has been updated with weights {self.weights}')
 
+        return self.df_grid_Livability
+
+
 
 
 
 if __name__ == '__main__':
-    # city = LivabilityMap(location = 'berlin')
-    # city.calc_livability()
-    # print(city.df_grid_Livability.info())
+    city = LivabilityMap(location = 'dresden')
+    city.calc_livability()
+    print(city.df_grid_Livability.info())
 
 
-    cities = [
-            'berlin',
-            'dresden',
-            'montpellier',
-            'paris',
-            'utrecht']
+    # cities = [
+    #         'berlin',
+    #         'dresden', [51.0763286,13.7726932]
+    #         'montpellier', geo:43.6100331,3.8741902
+    #         'paris', geo:48.8588657,2.3469411
+    #         'utrecht'] geo:51.8546693,4.4759326
 
-    cities2 = [
-            'kiel',
-            'budapest',
-            'milano',
-            'napoli',
-            'dublin']
+    # cities2 = [
+    #         'kiel', geo:54.3418225,10.125773
+    #         'budapest', geo:47.4814183,19.1300157
+    #         'milano', geo:45.4612932,9.1594985
+    #         'napoli', geo:40.853658,14.242934
+    #         'dublin'] geo:53.0978939,-8.1676402
 
-    cities3 = [
-            'zagreb',
-            'lisboa',
-            'erfurt',
-            'riga',
-            'Nairobi']
+    # cities3 = [
+    #         'zagreb', geo:45.8426723,15.964395
+    #         'lisboa', geo:38.7440789,-9.1580842
+    #         'erfurt', geo:50.9851833,11.0149019
+    #         'Tallinn', 59.4400774, 24.7592786
+    #         'Nairobi']geo:-1.3039015,36.8774125
 
-    cities4 = [
-            'london',
-            'Portland',
-            'Montevideo',
-            'Quito',
-            'Cairo',
-            'Melbourne']
+    # cities4 = [
+    #         'london',
+    #         'Portland', geo:45.5427086,-122.654387
+    #         'Montevideo', geo:-34.8933651,-56.183324
+    #         'Quito',geo:-0.1697022,-78.5562927
+    #         'Cairo', 30.0443879, 31.2357257
+    #         'Melbourne'] geo:-37.8274865,144.970266
 
 
-    cities = [
-            # 'berlin',
-            # 'dresden',
-            # 'cologne',
-            # 'munich',
-            # 'Barcelona'#,
-            #   'madrid',
-            #   'alicante',
-            #   'paris',
-            #   'London',
-            #   'rome',
-            #   'milan',
-            #   'erfurt',
-            #   'aukland',
-            #   'riga',
-            #   'rio de janeiro',
-            #   'sao paulo',
-            #   'buenos aires',
-            # 'montevideo',
-            'London',
-            'seatle',
-            #   'new york',
-            # 'tokyo'
-              ]
-    for city in cities:
-        map_city = LivabilityMap(location = city)
-        map_city.calc_livability()
-        print(map_city.df_grid_Livability.info())
+    # for city in cities2:
+    #     map_city = LivabilityMap(location = city)
+    #     map_city.calc_livability()
+    #     print(map_city.df_grid_Livability.info())
