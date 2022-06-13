Fixed bug in creating grid (Paris, Milano, etc. Bug was in cosine of latitude. Latitude must be radians instead of decimal). Changed input text to drop down menu

app.py
@@ -1,3 +1,4 @@
+from operator import index
 import streamlit as st
 import folium
 from folium import GeoJson
@@ -8,6 +9,9 @@ import pandas as pd
 from config.config import ROOT_DIR
 from livablestreets.display_map import plot
 from livablestreets.generator import LivabilityMap
+from livablestreets.params import preloaded_cities
+
+
 #-----------------------page configuration-------------------------
 st.set_page_config(
     page_title="Livable Streets",
@@ -94,13 +98,19 @@ weight_dict={"Don't care":0,
              'Average':0.5,
              'Quite important':0.75,
              'Very important':1}
+
 with st.sidebar:
     form = st.form("calc_weights")
-    form.text_input(label='Type city name', key='input_city', type="default", on_change=None, placeholder='p.ex. Berlin')
-    form.select_slider(label='Activity options', options=list(weight_dict.keys()), value='Average', key='weight_activity', help=None, on_change=None)
-    form.select_slider(label='Comfort', options=list(weight_dict.keys()), value='Average', key='weight_comfort', help=None, on_change=None)
-    form.select_slider(label='Mobility around the city', options=list(weight_dict.keys()), value='Average', key='weight_mobility', help=None, on_change=None)
-    form.select_slider(label='Social aspects', options=list(weight_dict.keys()), value='Average', key='weight_social', help=None, on_change=None)
+
+    # City selection
+    form.selectbox(label = 'Select a city of interest', key='input_city', options = preloaded_cities, index=0)
+    # form.text_input(label='Type city name', key='input_city', type="default", on_change=None, placeholder='p.ex. Berlin')
+
+    # Weights selection
+    form.select_slider(label='Activities and services:', options=list(weight_dict.keys()), value='Average', key='weight_activity', help=None, on_change=None)
+    form.select_slider(label='Comfort:', options=list(weight_dict.keys()), value='Average', key='weight_comfort', help=None, on_change=None)
+    form.select_slider(label='Mobility:', options=list(weight_dict.keys()), value='Average', key='weight_mobility', help=None, on_change=None)
+    form.select_slider(label='Social life:', options=list(weight_dict.keys()), value='Average', key='weight_social', help=None, on_change=None)
 
 
     #Form submit button to generate the inputs from the user
@@ -122,6 +132,7 @@ if submitted:
     df = city.df_grid_Livability
     #city center position lat,lon
     city_coords = [np.mean(df['lat_center']),np.mean(df['lng_center'])]
+    print(f"""============ {city.location} coordinates: {city_coords} =============""")
 
     #for filling the polygon
     style_function = {'fillColor': 'transparent',

livablestreets/create_grid.py
@@ -34,6 +34,7 @@ def get_id_deambiguate(location):
     area_osm_id = int(city.raw.get("osm_id")) #for city
     return area_osm_id
 
+@simple_time_tracker
 def get_city_geojson(area_osm_id):
 
     #overpass query with overpy
@@ -73,8 +74,7 @@ def save_geojson(location, location_name):
     with open(f'{ROOT_DIR}/livablestreets/data/{location_name}/{location_name}_boundaries.geojson', 'w') as f:
         dump(mapping(city_shape), f)
 
-'''----------------------------------------------------'''
-
+@simple_time_tracker
 def get_shape_of_location(location, location_name):
     """ Receives a location and returns the shape, if that location is in the data base (raw_data)"""
     try:
@@ -90,6 +90,7 @@ def get_shape_of_location(location, location_name):
     print(f'Obtained shape file for {location_name}')
     return gdf
 
+@simple_time_tracker
 def calculate_features_from_centroid(df, location, location_name, location_polygon = None):
     """ Iterates through all rows of grid squares and measures the distance (in km) to the location centroid (geographic center), angle (in degrees) and if point belongs to polygon shape """
 
@@ -153,19 +154,20 @@ def create_geofence(location, location_name, stepsize,
                     save_local=True, save_gcp=False):
     # Obtain location max bounds
     if north_lat is None:
-        east_lng, north_lat, west_lng, south_lat = get_shape_of_location(location, location_name=location_name)['geometry'].total_bounds
+        east_lng, south_lat, west_lng, north_lat = get_shape_of_location(location, location_name=location_name)['geometry'].total_bounds
 
     print(f'Bounds coordinates are north_lat:{north_lat}, south_lat:{south_lat}, east_lng:{east_lng}, west_lng:{west_lng}')
 
     # Adjust setpsize
     if stepsize > 10: # This probably means that the step size is in meters and should be converted to degrees
-        stepsize_x = m_to_coord(stepsize, latitude=north_lat, direction='x')
+        stepsize_x = m_to_coord(stepsize, latitude=np.mean([north_lat,south_lat]), direction='x')
         stepsize_y = m_to_coord(stepsize, direction='y')
         print(f'Stepsize of {stepsize}m converted to step sizes of lat={stepsize_y} and lng={stepsize_x} degrees')
     else:
         stepsize_x = stepsize
         stepsize_y = stepsize
 
+    ## Build the grid table
     # Obtain start and end of each grid individually
     lat_start = []
     lat_end = []
@@ -174,7 +176,7 @@ def create_geofence(location, location_name, stepsize,
     lat_center = []
     lng_center = []
 
-    for y in np.arange(north_lat, south_lat-stepsize_y, stepsize_y)[::-1]:
+    for y in np.arange(south_lat, north_lat-stepsize_y, stepsize_y)[::-1]:
         for x in np.arange(east_lng, west_lng-stepsize_x, stepsize_x):
             lat_start.append(y)
             lat_end.append(y+stepsize_y)
@@ -207,4 +209,4 @@ def create_geofence(location, location_name, stepsize,
 
 
 if __name__ == '__main__':
-    print(create_geofence(location = 'berlin', stepsize = 3000))
+    print(create_geofence(location_name = 'milano', location='Milano', stepsize = 1000))

livablestreets/utils.py
@@ -106,6 +106,7 @@ def m_to_coord(m, latitude=52.52, direction='east'):
 
     # Coordinate offsets in radians
     if direction in ['x', 'east', 'west', 'e', 'w']:
+        latitude = math.pi*latitude/180 # Convert latitude to raians
         return abs(m/(111_111*math.cos(latitude)))
     elif direction in ['y', 'north', 'south', 'n', 's']:
         return abs(m/111_111)
@@ -150,4 +151,5 @@ def simple_time_tracker(method):
     return timed
 
 if __name__ == '__main__':
-    pd.read_csv('/home/carlo/code/chvieira2/livablestreets/livablestreets/data/berlin/WorkingTables/Livability_berlin_grid_1000m.csv').info()
+    print(m_to_coord(1000, latitude=np.mean([45.5358482,45.3867381]), direction='x'))
+    print([math.pi*(math.cos(x)/180) for x in np.arange(45,55,1)])